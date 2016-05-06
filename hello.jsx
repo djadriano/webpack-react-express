aa5796053262c196db8f1c86f9fbec3e8ff6e746
Adriano Fernandes <adrianof@lt-r13-sp-005.ny.rga.com>
2016-05-06 11:07:21 -0300

    ...

--- hello.jsx
+++ hello.jsx
@@ -3,6 +3,6 @@ var React = require('react');
 module.exports = React.createClass({
   displayName: 'HelloReact',
   render: function(){
-      return <div>Hello Reacts Drica</div>
+      return <div>Hello React</div>
   }
 });
