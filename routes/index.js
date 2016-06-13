f75ccaa2d0ac7d0875dd2b08250503f936d65a35
Adriano Fernandes <adrianof@lt-r13-sp-005.ny.rga.com>
2016-06-13 16:25:13 -0300

    ...

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 module.exports = router;
