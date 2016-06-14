46847adadca037eadc1253c15c93004db7442dac
Adriano Fernandes <djadrianof@gmail.com>
2016-06-13 22:41:21 -0300

    ...

--- client/source/javascripts/posts.jsx
+++ client/source/javascripts/posts.jsx
@@ -55,7 +55,7 @@ export default class PostsComponent extends React.Component {
     let buttonText = (this.state.loading ? 'Loading...' : 'Load more');
 
     if(!this.state.posts.length) {
-      return (<span>Loading posts...</span>);
+      return (<div className="gabba-loading">Loading posts...</div>);
     }
 
     return(
